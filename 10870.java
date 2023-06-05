d0b5ab0e882b956591784656e688ac19865327ee
조주희 <carla_cho@naver.com>
2023-06-06 01:05:31 +0900

    10870번

---
+++ 10870.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int [] array = new int[100];
+        array[0]=0;
+        array[1]=1;
+        array[2]=1;
+        for(int i=3; i<array.length; i++){
+            array[i] = array[i-1]+array[i-2];
+        }
+        System.out.println(array[n]);
+    }
+}
