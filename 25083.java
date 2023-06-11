2d083ea5fcb0daad0e55752e6f9fc55a520ff462
조주희 <carla_cho@naver.com>
2023-06-11 11:10:41 +0900

    2023-06-11

---
+++ 25083.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    	System.out.print("         ,r'\"7\n");
+    	System.out.println("r`-_   ,'  ,/");
+    	System.out.println(" \\. \". L_r'");
+    	System.out.println("   `~\\/");
+    	System.out.println("      |");
+    	System.out.println("      |");
+    }
+}
