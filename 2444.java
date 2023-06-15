bf3226195be8347f568bdc46cc1f9444338a92f1
조주희 <carla_cho@naver.com>
2023-06-15 22:41:25 +0900

    2023-06-15

---
+++ 2444.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        
+        for (int i=1; i<=n; i++) {
+            for (int j=0; j<n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k=0; k<2*i-1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i=n-1; i>=0; i--) {
+            for (int j=0; j<n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k=0; k<2*i-1; k++) {
+                System.out.print("*");
+            }
+            if (i != 0) {
+				System.out.println();
+			}
+        }
+    }
+}
