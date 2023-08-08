da73cab0cb27e146b409c1e8f9a6076d5c529d4b
조주희 <carla_cho@naver.com>
2023-08-09 00:01:26 +0900

    2023-08-07

---
+++ 1940.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+   
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int [] arr = new int[15004];
+		int count = 0, sum = 0;
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		//nC2
+		for(int i=0; i<N; i++) { //arr[i]			
+			for(int j=i+1; j<N; j++) { //arr[j]
+				if(arr[i] + arr[j] == M) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
