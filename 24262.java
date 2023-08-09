d411717d850ae0b81e654e37d5b62d8c29e31130
JOOHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2023-08-10 01:00:50 +0900

    2023-06-27

--- 24262.java
+++ 24262.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {    	
        Scanner s = new Scanner(System.in);
 
-	   System.out.println("1");
-       System.out.println("0");
+	System.out.println("1");
+	System.out.println("0");
     }  
 }
