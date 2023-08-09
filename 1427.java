db3c091098631668d048ff14f7606c88dc26ea74
JOOHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2023-08-10 00:59:19 +0900

    2023-06-30

--- 1427.java
+++ 1427.java
@@ -10,12 +10,11 @@ public class Main {
     	int [] arr = new int[num.length];
     	
     	for (int i=0; i<num.length; i++) {
-			arr[i] = Integer.parseInt(num[i]);
-		}
-    	Arrays.sort(arr);
-		
-		for (int i=arr.length-1; i>=0; i--) {
-			System.out.print(arr[i]);
-		}
+		arr[i] = Integer.parseInt(num[i]);
+	}
+    	Arrays.sort(arr);		
+	for (int i=arr.length-1; i>=0; i--) {
+		System.out.print(arr[i]);
+	}
     }
 }
