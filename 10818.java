1bc4f76d6d554c4b841ff77e711e7fd47af1a11a
JOOHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2023-08-10 00:51:19 +0900

    2023-06-20

--- 10818.java
+++ 10818.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
     	Scanner s = new Scanner(System.in);
     	int n = s.nextInt();
-		int [] arr = new int[n];
+	int [] arr = new int[n];
 		
     	for(int i=0; i<n; i++) {
     		arr[i] = s.nextInt();
     	}
     	int min = arr[0];
-		int max = arr[0];
-		for(int i=0; i<n; i++) {
+	int max = arr[0];
+	for(int i=0; i<n; i++) {
     		if(min > arr[i])
     			min = arr[i];
     		if(max < arr[i])
