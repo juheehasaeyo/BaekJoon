8ad47baaca1d2a7ef722f666fc39be1bc3c0c1e8
JOOHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2023-08-10 01:01:49 +0900

    2023-06-27

--- 24264.java
+++ 24264.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {    	
        Scanner s = new Scanner(System.in);
 
-       long n = s.nextLong();
+	long n = s.nextLong();
        
-	   System.out.println(n*n);
-       System.out.println("2");
+	System.out.println(n*n);
+	System.out.println("2");
     }  
 }
