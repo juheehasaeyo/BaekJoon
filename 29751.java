0d76ae2a38635c5aacf219d477e8d44d91241d4c
JOOHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2023-10-02 23:33:25 +0900

    2023-10-02

---
+++ 29751.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+//29751
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        int h = sc.nextInt();    
+        System.out.printf("%.1f\n" , w*h*0.5);       
+    }
+}
