b0d3accb58d27c20fcab547a52511351050f1504
조주희 <carla_cho@naver.com>
2023-10-22 23:58:10 +0900

    2023-10-22

---
+++ 14645.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+//14645
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
