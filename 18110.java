4048739ae135350eab6a76f57e3176e00019e31b
조주희 <carla_cho@naver.com>
2023-10-28 23:59:56 +0900

    2023-10-28

---
+++ 18110.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        int num = (int)Math.round(n * 0.15);
+        double sum = 0; // int로 선언하면 /by zero 오류 뜸
+        for(int i=num; i<n-num; i++) {
+            sum += arr[i];
+        }
+        System.out.println((int)Math.round(sum/(n-num*2)));
+    }
+}
