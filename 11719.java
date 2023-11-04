2547e05f6568473441fae2ffd1971125104a520e
조주희 <carla_cho@naver.com>
2023-11-04 23:38:27 +0900

    2023-11-04

---
+++ 11719.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+// 11719 그대로 출력하기 2
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
