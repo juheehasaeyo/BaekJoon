6f92aef52d8aa398d421895c43979188cf1e93b8
조주희 <carla_cho@naver.com>
2023-11-16 23:47:45 +0900

    2023-11-16

---
+++ 14935.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+// 14935 FA
+// 모든 수는 FA로 귀결된다
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        System.out.println("FA");
+    }
+}
