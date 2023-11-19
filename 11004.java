549afe243ff583ea39acde1555813db7c72716e6
조주희 <carla_cho@naver.com>
2023-11-19 14:37:52 +0900

    2023-11-19

---
+++ 11004.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 11004 K번째 수
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        bw.write(Integer.toString(arr[k-1]));
+        bw.flush();
+        bw.close();
+    }
+}
