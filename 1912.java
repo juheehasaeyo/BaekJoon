b9146906125be23eba62b071ce903968ffe35aec
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2023-11-22 01:50:00 +0900

    2023-11-21

--- 1912.java
+++ 1912.java
@@ -18,3 +18,29 @@ public class Main {
         System.out.println(max);
     }
 }
+/*
+import java.io.*;
+// 1912 연속합
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] psum = new int[n+4];
+        String[] input = br.readLine().split(" ");
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        int max = arr[0];
+        psum[0] = arr[0];
+        for(int i=1; i<n; i++) {
+            psum[i] = Math.max(arr[i], psum[i-1] + arr[i]);
+            max = Math.max(max, psum[i]);
+        }
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+    }
+}
+*/
