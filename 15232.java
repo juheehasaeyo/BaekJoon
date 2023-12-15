2c5f362dee40ce74d99b76f5a6defaca33762f96
조주희 <carla_cho@naver.com>
2023-12-15 23:36:51 +0900

    2023-12-15

---
+++ 15232.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+// 15232 Rectangles
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
