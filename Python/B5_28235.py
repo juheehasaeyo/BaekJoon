c692a11d3ca31669f96cc966ccdc27161c9d4f4e
조주희 <carla_cho@naver.com>
2024-02-23 23:51:38 +0900

    2024-02-23

---
+++ Python/B5_28235.py
@@ -0,0 +1,10 @@
+# 28235 코드마스터 2023
+str = input()
+if str == "SONGDO":
+    print("HIGHSCHOOL")
+elif str == "CODE":
+    print("MASTER")
+elif str == "2023":
+    print("0611")
+elif str == "ALGORITHM":
+    print("CONTEST")
