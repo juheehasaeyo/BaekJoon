b29d4ade1928724f7a206ab425e4ab2785a0a7c7
조주희 <carla_cho@naver.com>
2024-03-11 19:28:54 +0900

    2024-03-11

--- Python/B2_11596.py
+++ Python/B2_11596.py
@@ -8,3 +8,15 @@ if (arr1[0]**2 + arr1[1]**2 == arr1[2]**2) and (arr2[0]**2 + arr2[1]**2 == arr2[
     print("YES")
 else:
     print("NO")
+
+# 11596 Triangle
+# arr1 = list(map(int, input().split()))
+# arr2 = list(map(int, input().split()))
+# arr1.sort()
+# arr2.sort()
+# # 피타고라스 정리 만족 + 각 변의 길이 같은지 확인
+# if (arr1[0]**2 + arr1[1]**2 == arr1[2]**2) and (arr2[0]**2 + arr2[1]**2 == arr2[2]**2)\
+#         and arr1 == arr2:
+#     print("YES")
+# else:
+#     print("NO")
