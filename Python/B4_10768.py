df79e65452f1446ad3113b194fde49080d490caa
조주희 <carla_cho@naver.com>
2024-03-19 23:50:16 +0900

    2024-03-19

---
+++ Python/B4_10768.py
@@ -0,0 +1,14 @@
+# 10768 특별한 날
+month = int(input())
+day = int(input())
+if month < 2:
+    print("Before")
+elif month == 2:
+    if day == 18:
+        print("Special")
+    elif day > 18:
+        print("After")
+    else:
+        print("Before")
+else:
+    print("After")
