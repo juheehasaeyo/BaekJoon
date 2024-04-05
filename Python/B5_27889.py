6c3faaa772ba461539416b6fd12c5103133f0ef1
조주희 <carla_cho@naver.com>
2024-04-06 01:48:31 +0900

    2024-04-05

---
+++ Python/B5_27889.py
@@ -0,0 +1,10 @@
+# 27889 특별한 학교 이름
+str = input()
+if str == "NLCS":
+    print("North London Collegiate School")
+elif str == "BHA":
+    print("Branksome Hall Asia")
+elif str == "KIS":
+    print("Korea International School")
+elif str == "SJA":
+    print("St. Johnsbury Academy")
