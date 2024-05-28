ac6b23f731e9273298d2fc2342a63942c7ade693
조주희 <carla_cho@naver.com>
2024-05-28 12:38:24 +0900

    2024-05-28

---
+++ Python/B4_4714.py
@@ -0,0 +1,7 @@
+# 4714 Lunacy
+while True:
+    x = float(input())
+    if x == -1:
+        break
+    y = x * 0.167
+    print(f"Objects weighing {x:.2f} on Earth will weigh {y:.2f} on the moon.")
